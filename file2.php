dfc34d3d00de07c0bfdad2a76a43438f457039f6
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-24 17:31:04 +0100

    Piccola decorazione in Css

--- file2.php
+++ file2.php
@@ -5,33 +5,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP intro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+
+    <h1>
+        Paragrafo scritto:
+    </h1>
     <h2>
         <?php
         echo $_GET['paragrafo'];
         ?>
     </h2>
-
-    <h3>
-        <?php
-        echo $_GET['parola'];
-        ?>
-    </h3>
-
+    <h1>
+        Parola da censurare:
+    </h1>
     <h2>
         <?php
-        echo str_replace($_GET['parola'], '***', $_GET['paragrafo']) . strlen($_GET['paragrafo']);
-        ?>
-    </h2>
-
-    <h3>
-        <?php
         echo $_GET['parola'];
         ?>
-    </h3>
+    </h2>
+    <hr>
+    <h1>
+        Risultato:
+    </h1>
+    <h2>
+        <?php
+        echo str_replace($_GET['parola'], '***', $_GET['paragrafo']);
+        ?>
+    </h2>
+    <h1>
+        Lunghezza del paragrafo:
+    </h1>
+    <h2>
+        <?php
+        echo strlen($_GET['paragrafo']);
+        ?>
+    </h2>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -6,13 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP intro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <main>
         <form action="./file2.php" method="GET">
-            <input type="text" name="paragrafo">
-            <input type="text" name="parola">
+            <input type="text" name="paragrafo" placeholder="Scrivi il tuo paragrafo qui!">
+            <input type="text" name="parola" placeholder="Scrivi la parola da censurare nel tuo paragrafo">
             <button type="submit">INVIA</button>
         </form>
     </main>
