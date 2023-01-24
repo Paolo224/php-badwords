23c25e8eac53faaa1dbb1981bb1c72191ec22930
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-24 17:09:49 +0100

    Esercizio completo

--- file2.php
+++ file2.php
@@ -1,23 +1,37 @@
-<h2>
-    <?php
-    echo $_GET['paragrafo'];
-    ?>
-</h2>
+<!DOCTYPE html>
+<html lang="en">
 
-<h3>
-    <?php
-    echo $_GET['parola'];
-    ?>
-</h3>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-<h2>
-    <?php
-    echo str_replace($_GET['parola'], '***', $_GET['paragrafo']);
-    ?>
-</h2>
+<body>
+    <h2>
+        <?php
+        echo $_GET['paragrafo'];
+        ?>
+    </h2>
 
-<h3>
-    <?php
-    echo $_GET['parola'];
-    ?>
-</h3>
+    <h3>
+        <?php
+        echo $_GET['parola'];
+        ?>
+    </h3>
+
+    <h2>
+        <?php
+        echo str_replace($_GET['parola'], '***', $_GET['paragrafo']) . strlen($_GET['paragrafo']);
+        ?>
+    </h2>
+
+    <h3>
+        <?php
+        echo $_GET['parola'];
+        ?>
+    </h3>
+</body>
+
+</html>
